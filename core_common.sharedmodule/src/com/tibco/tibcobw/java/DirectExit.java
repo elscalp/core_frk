888f6d078d697206a3679651d9f5377df7b31e60
elscalp <el.hicham@gmail.com>
2016-10-31 23:19:47 +0100

    1st version

---
+++ core_common.sharedmodule/src/com/tibco/tibcobw/java/DirectExit.java
@@ -0,0 +1,14 @@
+package com.tibco.tibcobw.java;
+
+public class DirectExit{
+/****** START SET/GET METHOD, DO NOT MODIFY *****/
+/****** END SET/GET METHOD, DO NOT MODIFY *****/
+	public DirectExit() {
+	}
+	public void invoke() throws Exception {
+/* Available Variables: DO NOT MODIFY
+* Available Variables: DO NOT MODIFY *****/
+System.exit(0);
+
+}
+}
